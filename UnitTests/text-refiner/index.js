0fa27fc904b7417a66ffe296af5fb9d9c1bcff91
seongho.hong <hsh3592@gmail.com>
2021-03-28 22:16:33 +0900

    text refiner 리팩토링

--- UnitTests/text-refiner/index.js
+++ UnitTests/text-refiner/index.js
@@ -1,20 +1,28 @@
-function refineText(s, options) {
-    s = s
-        .replace("     ", " ")
-        .replace("\t", " ")
-        .replace("    ", " ")
-        .replace("   ", " ")
-        .replace("  ", " ")
-        .replace("mockist", "*******")
-        .replace("purist", "******");
+function refineText(source, options) {
+    return [normalizeWhiteSpaces, compactWhiteSpaces, maskBannedWords].reduce(
+        (value, filter) => filter(value, options),
+        source
+    );
+}
 
+function maskBannedWords(source, options) {
     if (options) {
-        for (const bannedWord of options.bannedWords) {
-            s = s.replace(bannedWord, "*".repeat(bannedWord.length));
-        }
+        return options.bannedWords.reduce(maskBannedWord, source);
     }
+    return source;
+}
 
-    return s;
+function maskBannedWord(source, bannedWord) {
+    const mask = "*".repeat(bannedWord.length)
+    return source.replace(bannedWord, mask);
+}
+
+function normalizeWhiteSpaces(source) {
+    return source.replace("\t", " ");
+}
+
+function compactWhiteSpaces(source) {
+    return source.indexOf("  ") < 0 ? source : compactWhiteSpaces(source.replace("  ", " "));
 }
 
 module.exports = refineText;
